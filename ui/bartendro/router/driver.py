7c3769f989ad7b39b9558b93028b3a3c5e12d6c5
Lucas Noah <lucas.bird.noah@gmail.com>
2018-02-01 15:29:23 -0800

    inform me

--- ui/bartendro/router/driver.py
+++ ui/bartendro/router/driver.py
@@ -533,7 +533,7 @@ class RouterDriver(object):
             return False
         print "about to send that packet"
         print "send packet dest", type(dest), dest
-        print "send packet packed up", type(pack("BBBBBB", dispenser_id, in_type, val0, val1, val2, val3)), pack("BBBBBB", dispenser_id, in_type, val0, val1, val2, val3)
+        print "send packet packed up", type(pack("BBBBBB", dispenser_id, in_type, val0, val1, val2, val3)), pack("BBBBBB", dispenser_id, in_type, val0, val1, val2, val3).decode()
 
 
         return self._send_packet(dest, pack("BBBBBB", dispenser_id, in_type, val0, val1, val2, val3))
